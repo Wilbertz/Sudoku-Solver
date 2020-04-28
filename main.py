0da0bbf511764df2043f9a147cfe2de5fba693f0
Harald Wilbertz <Harald.Wilbertz@gmx.de>
2020-04-28 18:44:48 +0200

    Solver class added.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-def class solver:
-    pass
-
 # The main startup code.
+from solver import Solver
+
 def main():
     print("python main function started.")
-
+    solver = Solver()
 
 if __name__ == '__main__':
     main()

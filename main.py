9bb50d9e3a5595189e0b40e7ed1c59959455ddce
Harald Wilbertz <harald.wilbertz@gmx.de>
2020-09-03 20:56:47 +0200

    Code reformatted.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # The main startup code.
 from dpll_algorithm import DpllAlgorithm
 
+
 def main():
     print("python main function started.")
     solver = DpllAlgorithm()
+
 
 if __name__ == '__main__':
     main()

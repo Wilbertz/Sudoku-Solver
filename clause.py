5ae1c7780872595363d3b79e5fb04526e3b56910
Harald Wilbertz <harald.wilbertz@gmx.de>
2020-11-16 19:30:04 +0100

    Constructor extended.

--- clause.py
+++ clause.py
@@ -1,10 +1,11 @@
 """This class encodes a clause."""
+from typing import List
 
 
 class Clause:
-    def __init__(self):
-        self.Variables = []
-        self.NegatedVariables = []
+    def __init__(self, variables: List[int], negated_variables: List[int]):
+        self.Variables = variables
+        self.NegatedVariables = negated_variables
         self.IsUnsat = False
 
     def substitute_as_true(self, index: int):

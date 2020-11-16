9034caea6a50c13f354e5487f6e35ef62f7f0abc
Harald Wilbertz <harald.wilbertz@gmx.de>
2020-11-16 19:37:54 +0100

    Constructor arguments added.

--- formula.py
+++ formula.py
@@ -1,9 +1,11 @@
 """This class encodes a formula."""
+from typing import List
+from clause import Clause
 
 
 class Formula:
-    def __init__(self):
-        pass
+    def __init__(self, clauses: List[Clause]):
+        self.clauses = clauses
 
 
 if __name__ == '__main__':

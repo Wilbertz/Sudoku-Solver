3b9bf9e1ba9e4431eb27036ddea7b2f129ceff3e
Harald Wilbertz <harald.wilbertz@gmx.de>
2020-11-16 19:25:18 +0100

    Constructor extended.

--- clause.py
+++ clause.py
@@ -3,9 +3,13 @@
 
 class Clause:
     def __init__(self):
-        pass
+        self.Variables = []
+        self.NegatedVariables = []
+        self.IsUnsat = False
 
     def substitute_as_true(self, index: int):
+        if index in self.Variables:
+            pass
         pass
 
     def substitute_as_false(self, index: int):

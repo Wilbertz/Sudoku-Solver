2bb97a599c46159bffcac230b77ed30fd00b96fc
Harald Wilbertz <harald.wilbertz@gmx.de>
2020-11-18 19:26:14 +0100

    Unittests improved.

--- tests/clause_test.py
+++ tests/clause_test.py
@@ -1,17 +1,24 @@
 """Unit tests for clause class."""
-
+import logging
 import unittest
 from clause import Clause
 
 
-class TestFormula(unittest.TestCase):
-    def test_dummy(self):
-        self.assertTrue
+class TestClause(unittest.TestCase):
 
-    def test_formula_constructor(self):
+    def test_dummy(self):
+        self.assertTrue(True)
+
+    def test_clause_constructor_should_accept_empty_argument_sets(self):
         clause = Clause(set(), set())
         self.assertTrue(clause is not None)
 
+    def test_clause_constructor_should_detect_inconsistent_arguments(self):
+        with self.assertRaises(Exception) as context:
+            clause = Clause({1}, {1})
+        self.assertTrue('Variables and NegatedVariables cannot contain a common element' in str(context.exception))
+
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     unittest.main()
